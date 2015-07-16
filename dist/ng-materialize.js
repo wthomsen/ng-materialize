db854449bf77604ff53c534a6f3bef07e84f1ef9
William Thomsen <wthomsen@Williams-MBP.davidbild.org>
2015-07-16 18:56:04 -0500

    perf(tabs) removed window resize watch requirement

--- dist/ng-materialize.js
+++ dist/ng-materialize.js
@@ -1038,131 +1038,148 @@ angular.module('template/modal/window.html', []).run(['$templateCache', function
 
 'use strict';
 
-angular.module('ngMaterialize.tabs', ['template/tabs/tabset.html', 'template/tabs/tab.html'])
-  .controller('TabsetController', ['$scope', '$timeout', '$animate', '$window', function ($scope, $timeout, $animate, $window) {
-    var ctrl = this;
-    var tabs = ctrl.tabs = $scope.tabs = [];
-    var prevTab;
-    var activeTab;
-    $scope.reverse = false;
+    angular.module('ngMaterialize.tabs', ['template/tabs/tabset.html', 'template/tabs/tab.html'])
+      .controller('TabsetController', ['$scope', '$timeout', '$animate', '$window', function ($scope, $timeout, $animate, $window) {
+        var ctrl = this;
+        var tabs = ctrl.tabs = $scope.tabs = [];
+        var prevTab;
+        var activeTab;
+        var indicatorTimeout;
+        $scope.reverse = false;
 
-    ctrl.select = function(selectedTab) {
-      var prevTabIndex = 0;
-      var selectedTabIndex = 0;
-      angular.forEach(tabs, function(tab, i) {
-        if (tab.active && tab !== selectedTab) {
-          prevTab = tab;
-          tab.active = false;
-          tab.onDeselect();
-          prevTabIndex = i;
-        }
-        if (tab === selectedTab) {
-          selectedTabIndex = i;
-        }
-      });
+        ctrl.select = function(selectedTab) {
+          var prevTabIndex = 0;
+          var selectedTabIndex = 0;
+          angular.forEach(tabs, function(tab, i) {
+            if (tab.active && tab !== selectedTab) {
+              prevTab = tab;
+              tab.active = false;
+              tab.onDeselect();
+              prevTabIndex = i;
+            }
+            if (tab === selectedTab) {
+              selectedTabIndex = i;
+            }
+          });
 
-      $scope.reverse = selectedTabIndex < prevTabIndex;
+          $scope.reverse = selectedTabIndex < prevTabIndex;
 
-      selectedTab.onSelect();
-      selectedTab.active = true;
-      activeTab = selectedTab;
-      if (prevTab) {
-        updateIndicator(true);
-      } else {
-        $timeout(updateIndicator);
-      }
-    };
-
-    ctrl.addTab = function addTab(tab) {
-      tabs.push(tab);
-      // we can't run the select function on the first tab
-      // since that would select it twice
-      if (tabs.length === 1 && tab.active !== false) {
-        tab.active = true;
-      } else if (tab.active) {
-        ctrl.select(tab);
-      }
-      else {
-        tab.active = false;
-      }
-    };
-
-    ctrl.removeTab = function removeTab(tab) {
-      var index = tabs.indexOf(tab);
-      //Select a new tab if the tab to be removed is selected and not destroyed
-      if (tab.active && tabs.length > 1 && !destroyed) {
-        //If this is the last tab, select the previous tab. else, the next tab.
-        var newActiveIndex = index === tabs.length - 1 ? index - 1 : index + 1;
-        ctrl.select(tabs[newActiveIndex]);
-      }
-      tabs.splice(index, 1);
-    };
-
-    function calcIndicatorCss(targetTab) {
-      var tabsetWidth = $scope.tabsetElement[0].getBoundingClientRect().width;
-      var css = {left: 0, right: tabsetWidth};
-      var tabFound = false;
-      angular.forEach(tabs, function(tab) {
-        var tabWidth = tab.headingTranscludeElement[0].getBoundingClientRect().width;
-        if (!tabFound) {
-          css.right -= tabWidth;
-          if (tab !== targetTab) {
-            css.left += tabWidth;
+          selectedTab.onSelect();
+          selectedTab.active = true;
+          activeTab = selectedTab;
+          if (prevTab) {
+            updateIndicator(true);
           } else {
-            tabFound = true;
+            $timeout(updateIndicator);
+          }
+        };
+
+        ctrl.addTab = function addTab(tab) {
+          tabs.push(tab);
+          // we can't run the select function on the first tab
+          // since that would select it twice
+          if (tabs.length === 1 && tab.active !== false) {
+            tab.active = true;
+          } else if (tab.active) {
+            ctrl.select(tab);
+          }
+          else {
+            tab.active = false;
+          }
+        };
+
+        ctrl.removeTab = function removeTab(tab) {
+          var index = tabs.indexOf(tab);
+          //Select a new tab if the tab to be removed is selected and not destroyed
+          if (tab.active && tabs.length > 1 && !destroyed) {
+            //If this is the last tab, select the previous tab. else, the next tab.
+            var newActiveIndex = index === tabs.length - 1 ? index - 1 : index + 1;
+            ctrl.select(tabs[newActiveIndex]);
+          }
+          tabs.splice(index, 1);
+        };
+
+        function calcIndicatorCss(targetTab) {
+          var tabsetWidth = $scope.tabsetElement[0].getBoundingClientRect().width;
+          var css = {left: 0, right: tabsetWidth};
+          var tabFound = false;
+          angular.forEach(tabs, function(tab) {
+            var tabWidth = tab.headingTranscludeElement[0].getBoundingClientRect().width;
+            if (!tabFound) {
+              css.right -= tabWidth;
+              if (tab !== targetTab) {
+                css.left += tabWidth;
+              } else {
+                tabFound = true;
+              }
+            }
+          });
+          return css;
+        }
+
+        function setIndicatorCss(targetTab, prevTab) {
+          var indicator = $scope.indicator[0];
+          var tabset = $scope.tabsetElement[0];
+          var targetHeading = targetTab.headingTranscludeElement.parent()[0];
+          var start = calcIndicatorCss(prevTab);
+          var end = calcIndicatorCss(targetTab);
+          var transitionFrom = {
+            width: 'auto',
+            left: start.left + 'px',
+            right: start.right + 'px'
+          };
+          var transitionTo = {
+            width: 'auto',
+            left: end.left + 'px',
+            right: end.right + 'px'
+          };
+
+          var freezeTo = {
+            width: '100%',
+            left: 'auto',
+            right: 'auto'
+          };
+
+          if (prevTab) {
+            $animate.move(indicator, tabset, undefined, {to: transitionFrom, duration: 0}).then(function () {
+              $scope.indicator.css(transitionTo);
+              $timeout.cancel(indicatorTimeout);
+              indicatorTimeout = $timeout(function () {
+                $animate.move(indicator, targetHeading, undefined, {to: freezeTo});
+              }, 500);
+            });
+          } else {
+            $animate.move(indicator, targetHeading, undefined, {to: freezeTo, duration: 0});
+          }
+
+        }
+
+        function updateIndicator(animate) {
+          var toCss = calcIndicatorCss(activeTab);
+          var fromCss = prevTab ? calcIndicatorCss(prevTab) : toCss;
+          var direction = toCss.left > fromCss.left ? 'right' : 'left';
+          var transitionClass = 'indicator-' + direction;
+
+          if (!animate || !prevTab || $scope.indicator.hasClass(transitionClass)) {
+            $timeout(function () {
+              setIndicatorCss(activeTab, prevTab);
+            });
+          } else {
+            $scope.indicator.toggleClass('indicator-left', direction === 'left');
+            $scope.indicator.toggleClass('indicator-right', direction === 'right');
+            $timeout(function () {
+              setIndicatorCss(activeTab, prevTab);
+            });
           }
         }
-      });
-      return css;
-    }
 
-    function setIndicatorCss(targetTab) {
-      var css = calcIndicatorCss(targetTab);
-      $scope.indicator.css({
-        left: Math.round(css.left) + 'px',
-        right: Math.round(css.right) + 'px'
-      });
-    }
-
-    function updateIndicator(animate) {
-      var toCss = calcIndicatorCss(activeTab);
-      var fromCss = prevTab ? calcIndicatorCss(prevTab) : toCss;
-      var direction = toCss.left > fromCss.left ? 'right' : 'left';
-      var transitionClass = 'indicator-' + direction;
-
-      if (!animate || !prevTab || $scope.indicator.hasClass(transitionClass)) {
-        $timeout(function () {
-          setIndicatorCss(activeTab);
+        var destroyed;
+        $scope.$on('$destroy', function() {
+          destroyed = true;
         });
-      } else {
-        $scope.indicator.toggleClass('indicator-left', direction === 'left');
-        $scope.indicator.toggleClass('indicator-right', direction === 'right');
-        $timeout(function () {
-          setIndicatorCss(activeTab);
-        });
-      }
-    }
+      }])
 
-    function onResize() {
-      $scope.indicator.toggleClass('indicator-left', false);
-      $scope.indicator.toggleClass('indicator-right', false);
-      $timeout(function () {
-        updateIndicator(false);
-      });
-    }
-
-    var w = angular.element($window);
-    w.on('resize', onResize);
-
-    var destroyed;
-    $scope.$on('$destroy', function() {
-      destroyed = true;
-      w.off('resize', onResize);
-    });
-
-  }])
-
-  .directive('tabset', ["$timeout", function($timeout) {
+    .directive('tabset', ["$timeout", function($timeout) {
     return {
       restrict: 'EA',
       transclude: true,
@@ -1224,7 +1241,7 @@ angular.module('ngMaterialize.tabs', ['template/tabs/tabset.html', 'template/tab
           }
 
           scope.select = function() {
-            if ( !scope.disabled ) {
+            if (!scope.disabled) {
               scope.active = true;
             }
           };
